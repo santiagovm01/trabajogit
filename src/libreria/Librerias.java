b359af716808f172ec6245e48f81734771e7fdc7
santiagovm01 <osvi0610@gmail.com>
2024-04-02 22:15:14 +0200

    este comit añade atriibutos

--- src/libreria/Librerias.java
+++ src/libreria/Librerias.java
@@ -5,28 +5,8 @@ public class Librerias {
     // Atributos
     private List<Libro> libros;
 
-    /**
-     * Constructor de la clase Library.
-     */
-    public Librerias() {
-        this.libros = new ArrayList<>();
-    }
 
-    /**
-     * Este método añade un libro a la biblioteca.
-     *
-     * @param libro El libro a añadir.
-     */
-    public void addBook(Libro libro) {
-        this.libros.add(libro);
-    }
 
-    /**
-     * Este método devuelve todos los libros de la biblioteca.
-     *
-     * @return Una lista de libros.
-     */
-    public List<Libro> getBooks() {
-        return libros;
-    }
+
+
 }
